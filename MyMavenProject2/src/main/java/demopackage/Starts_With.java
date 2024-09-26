9460fdc0147133b2b196fb44ac01e035083c9753
Kripasnair2000 <kripa.s@dridatah.com>
2024-09-26 12:59:02 +0530

    New maven project

---
+++ MyMavenProject2/src/main/java/demopackage/Starts_With.java
@@ -0,0 +1,15 @@
+package demopackage;
+
+public class Starts_With {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Positive";
+		System.out.println(s1.startsWith("Pos"));
+		System.out.println(s1.startsWith("end"));
+		
+
+	}
+
+
+}
